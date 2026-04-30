03e617e6279fbe2265827d2e5a64007d262a0404
Darlynee <darl.zamorano@duocuc.cl>
2026-04-29 23:57:13 -0400

    feat: iniciando el micro de ventas

---
+++ ventas-service/src/main/java/com/cordillera/ventas/VentasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cordillera.ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasServiceApplication.class, args);
+	}
+
+}

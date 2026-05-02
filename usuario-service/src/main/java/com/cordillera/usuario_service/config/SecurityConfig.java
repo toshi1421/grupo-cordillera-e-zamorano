18da2f61a57b734b676fef6737c856af6d2add8b
Darlynee <darl.zamorano@duocuc.cl>
2026-05-01 23:47:28 -0400

    feat: nueva ruta

--- usuario-service/src/main/java/com/cordillera/usuario_service/config/SecurityConfig.java
+++ usuario-service/src/main/java/com/cordillera/usuario_service/config/SecurityConfig.java
@@ -1,13 +1,14 @@
 package com.cordillera.usuario_service.config;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -32,12 +33,11 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/autenticacion/**").permitAll()
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/usuarios/login", "/usuarios").permitAll()
-                .requestMatchers(HttpMethod.GET, "/usuarios/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/usuarios/{id}").permitAll() 
+                .requestMatchers(HttpMethod.GET, "/api/usuarios").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/usuarios/panel-gerente").hasAnyRole("GERENTE", "ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/usuarios/*/rol").hasRole("ADMIN")
                 .requestMatchers("/error").permitAll()
-                .requestMatchers(HttpMethod.GET, "/usuarios").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/usuarios/panel-gerente").hasAnyRole("GERENTE", "ADMIN")
-                .requestMatchers(HttpMethod.PUT, "/usuarios/*/rol").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);

130bf9ec2e6f2ab4f6372dc7fdf71b07170d692a
Darlynee <darl.zamorano@duocuc.cl>
2026-05-02 00:48:51 -0400

    feat: correcion de ruta

--- ventas-service/src/main/java/com/cordillera/ventas/clients/UsuarioClient.java
+++ ventas-service/src/main/java/com/cordillera/ventas/clients/UsuarioClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "usuario-service", url = "http://localhost:8081/api/usuarios")
+@FeignClient(name = "usuario-service", url = "http://localhost:8081/usuarios")
 public interface UsuarioClient {
     @GetMapping("/{id}")
     Object obtenerUsuarioPorId(@PathVariable("id") Long id);

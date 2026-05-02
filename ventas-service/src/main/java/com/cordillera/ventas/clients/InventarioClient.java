48da764369e7f5dd38353aa673b1682c26a7e5bd
Darlynee <darl.zamorano@duocuc.cl>
2026-05-02 00:55:08 -0400

    feat: quite el api de la ruta

--- ventas-service/src/main/java/com/cordillera/ventas/clients/InventarioClient.java
+++ ventas-service/src/main/java/com/cordillera/ventas/clients/InventarioClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "inventario-service", url = "http://localhost:8082/api/productos")
+@FeignClient(name = "inventario-service", url = "http://localhost:8082/productos")
 public interface InventarioClient {
     @PutMapping("/{id}/descontar")
     void descontarStock(@PathVariable("id") Long id, @RequestParam("cantidad") Integer cantidad);

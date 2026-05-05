a921de1b0d23ad572ac36af9ed2ac1b47a317aca
Darlynee <darl.zamorano@duocuc.cl>
2026-05-05 00:32:18 -0400

    feat: agregue jwtfilter y util

---
+++ ventas-service/src/main/java/com/cordillera/ventas/filter/JwtUtil.java
@@ -0,0 +1,50 @@
+package com.cordillera.ventas.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "TuClaveSecretaMuyLargaParaElGrupoCordillera"; 
+
+    private Key getSigningKey() {
+        byte[] keyBytes = SECRET_KEY.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extraerUsername(String token) {
+        return extraerClaim(token, Claims::getSubject);
+    }
+
+    public String extraerRol(String token) {
+        return extraerAllClaims(token).get("rol", String.class);
+    }
+
+    public boolean validarToken(String token) {
+        try {
+            return !extraerAllClaims(token).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private <T> T extraerClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extraerAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extraerAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}

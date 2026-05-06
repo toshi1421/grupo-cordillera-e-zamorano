8a8a50869a5dced71c408bb23235ba34c1757d6b
Darlynee <darl.zamorano@duocuc.cl>
2026-05-05 21:35:38 -0400

    chore: aplicar gitignore y limpiar archivos basura

--- inventario-service/src/main/java/com/cordillera/inventario_service/exception/StockInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package com.cordillera.inventario_service.exception;
-
-public class StockInsuficienteException extends RuntimeException {
-    public StockInsuficienteException(String producto, Integer solicitado, Integer disponible) {
-        super("Stock insuficiente para '" + producto + "'. Solicitaste " + solicitado + " pero solo quedan " + disponible + " unidades.");
-    }
-}
--- usuario-service/src/main/java/com/cordillera/usuario_service/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cordillera.usuario_service.repository;
-
-import com.cordillera.usuario_service.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	Usuario findByEmail(String email);
-	boolean existsByEmail(String email);
-}
